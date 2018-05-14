1f11452daf019472f710a13ff5698a731dae3106
Fabian Zeiher <fzeiher@gmail.com>
2018-05-14 12:21:13 +0200

    SAVE: switching pc

--- TodoApp/app/src/main/java/com/example/fzeih/todo_app/MainActivity.java
+++ TodoApp/app/src/main/java/com/example/fzeih/todo_app/MainActivity.java
@@ -1,13 +1,49 @@
 package com.example.fzeih.todo_app;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+    private ArrayList<String> tasks;
+    private EditText textBox;
+    private ListView listView;
+    private ArrayAdapter<String> arrayAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tasks = new ArrayList<>();
+        textBox = findViewById(R.id.textbox);
+        Button add_button = findViewById(R.id.add_button);
+        listView = findViewById(R.id.list_view);
+
+        arrayAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, tasks);
+        listView.setAdapter(arrayAdapter);
+
+        add_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                tasks.add(textBox.getText().toString());
+                arrayAdapter.notifyDataSetChanged();
+            }
+        });
+
+        listView.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View view) {
+                tasks.remove(listView.getPositionForView(view));
+                arrayAdapter.notifyDataSetChanged();
+                return true;
+            }
+        });
     }
 }
