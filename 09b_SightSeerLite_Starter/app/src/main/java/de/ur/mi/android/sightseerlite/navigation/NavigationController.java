444adfc731dca1b9c8df1b60550f188ae914f89f
Fabian Zeiher <fzeiher@gmail.com>
2018-06-06 14:48:01 +0200

    FEAT: sightseer feature complete. Missing request for permissions

--- 09b_SightSeerLite_Starter/app/src/main/java/de/ur/mi/android/sightseerlite/navigation/NavigationController.java
+++ 09b_SightSeerLite_Starter/app/src/main/java/de/ur/mi/android/sightseerlite/navigation/NavigationController.java
@@ -1,11 +1,11 @@
 package de.ur.mi.android.sightseerlite.navigation;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.location.Criteria;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
-import android.location.LocationProvider;
 import android.os.Bundle;
 
 import de.ur.mi.android.sightseerlite.config.AppConfig;
@@ -14,6 +14,11 @@ import de.ur.mi.android.sightseerlite.domain.PointOfInterest;
 public class NavigationController implements LocationListener {
 
     private static NavigationController mInstance;
+    private LocationManager locationManager;
+    private String bestProvider;
+    private Location location;
+    private PointOfInterest pointOfInterest;
+    private NavigationListener navigationListener;
 
     public static NavigationController getInstance(Context ctx) {
         if (mInstance == null) {
@@ -23,31 +28,54 @@ public class NavigationController implements LocationListener {
     }
 
     private NavigationController(Context context) {
+        locationManager = (LocationManager) context.getSystemService(Context.LOCATION_SERVICE);
+        setBestProvider();
+        location = locationManager.getLastKnownLocation(bestProvider);
+    }
 
+    private void setBestProvider() {
+        Criteria criteria = new Criteria();
+        criteria.setAccuracy(Criteria.ACCURACY_FINE);
+        criteria.setPowerRequirement(Criteria.POWER_MEDIUM);
+        criteria.setBearingRequired(true);
+        bestProvider = locationManager.getBestProvider(criteria, true);
     }
 
     public void setNavigationListener(NavigationListener navigationListener) {
-
+        this.navigationListener = navigationListener;
     }
 
     public void setTarget(PointOfInterest poi) {
-
+        pointOfInterest = poi;
+        NavigationDetail navigationDetail = getNavigationDetail();
+        navigationListener.onNavigationDetailChanged(navigationDetail);
     }
 
-    public void startNavigation() {
+    private NavigationDetail getNavigationDetail() {
+        float distance = location.distanceTo(pointOfInterest.getLocation());
+        float bearing = location.bearingTo(pointOfInterest.getLocation());
+        return new NavigationDetail("toPOI", distance, bearing);
+    }
 
+    @SuppressLint("MissingPermission")
+    public void startNavigation() {
+        locationManager.requestLocationUpdates(bestProvider, AppConfig.LOCATION_UPDATE_INTERVAL, AppConfig.LOCATION_DISTANCE_THRESHOLD, this);
     }
 
     public void stopNavigation() {
+        locationManager.removeUpdates(this);
     }
 
     public float getEstimatedDistanceForLocation(Location location) {
-        return -1;
+        return location.distanceTo(this.location);
     }
 
     @Override
     public void onLocationChanged(Location location) {
-
+        this.location = location;
+        if (navigationListener != null) {
+            navigationListener.onNavigationDetailChanged(getNavigationDetail());
+        }
     }
 
     @Override
